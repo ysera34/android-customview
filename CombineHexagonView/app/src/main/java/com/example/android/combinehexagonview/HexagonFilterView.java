9d65e81e9f6d5c46dba2f539d40d5d484c9a5cea
ysera34 <ycyoon34@gmail.com>
2017-01-21 02:26:20 +0900

    update combine hexagon filter view

--- CombineHexagonView/app/src/main/java/com/example/android/combinehexagonview/HexagonFilterView.java
+++ CombineHexagonView/app/src/main/java/com/example/android/combinehexagonview/HexagonFilterView.java
@@ -1,6 +1,7 @@
 package com.example.android.combinehexagonview;
 
 import android.content.Context;
+import android.content.res.TypedArray;
 import android.util.AttributeSet;
 import android.view.View;
 import android.view.animation.Animation;
@@ -23,6 +24,20 @@ public class HexagonFilterView extends LinearLayout {
 
     public HexagonFilterView(Context context, AttributeSet attrs) {
         super(context, attrs);
+        TypedArray a = context.getTheme().obtainStyledAttributes(
+                attrs,
+                R.styleable.HexagonFilterView,
+                0,0
+        );
+
+        try {
+            mHexagonImageSrc = a.getResourceId(R.styleable.HexagonFilterView_hexagonImageSrc, 0);
+            mHexagonCount = a.getInt(R.styleable.HexagonFilterView_hexagonCount, 0);
+            mHexagonLabel = a.getNonResourceString(R.styleable.HexagonFilterView_hexagonLabel);
+        } finally {
+            a.recycle();
+        }
+
         init(context);
     }
 
@@ -33,14 +48,20 @@ public class HexagonFilterView extends LinearLayout {
     private TextView mUnitTextView;
     private TextView mLabelTextView;
 
+    private int mHexagonImageSrc;
+    private int mHexagonCount;
+    private String mHexagonLabel;
 
     private void init(Context context) {
         mRootView = inflate(context, R.layout.hexagon_filter, this);
         mHexagonFilterImageLayout = (RelativeLayout) mRootView.findViewById(R.id.hexagon_filter_image_layout);
         mHexagonFilterImageView = (ImageView) mRootView.findViewById(R.id.hexagon_filter_image_view);
+        mHexagonFilterImageView.setImageResource(mHexagonImageSrc);
         mCountTextView = (TextView) mRootView.findViewById(R.id.count_text_view);
+        mCountTextView.setText(String.valueOf(mHexagonCount));
         mUnitTextView = (TextView) mRootView.findViewById(R.id.unit_text_view);
         mLabelTextView = (TextView) mRootView.findViewById(R.id.label_text_view);
+        mLabelTextView.setText(mHexagonLabel);
 
 //        mRootView.setOnClickListener(new OnClickListener() {
 //            @Override
@@ -58,6 +79,7 @@ public class HexagonFilterView extends LinearLayout {
     }
 
     private boolean mScaleState = false;
+    private String mFilterLabel;
 
     public boolean isScaleState() {
         return mScaleState;
@@ -65,6 +87,14 @@ public class HexagonFilterView extends LinearLayout {
 
     public void setScaleState(boolean scaleState) {
         mScaleState = scaleState;
+    }
+
+    public String getFilterLabel() {
+        return mFilterLabel;
+    }
+
+    public void setFilterLabel(String filterLabel) {
+        mFilterLabel = filterLabel;
     }
 
     public void scaleUpAnimate() {
