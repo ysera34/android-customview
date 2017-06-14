e03fed260633443d187d4da96687232d18b6df62
ysera34 <ycyoon34@gmail.com>
2017-06-14 21:31:03 +0900

    rotate image view pager adapter

--- RotateImageViewPager/app/src/main/java/com/example/android/rotateimageviewpager/MainActivity.java
+++ RotateImageViewPager/app/src/main/java/com/example/android/rotateimageviewpager/MainActivity.java
@@ -3,11 +3,28 @@ package com.example.android.rotateimageviewpager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
+
+    private RotateImageViewPager mRotateImageViewPager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mRotateImageViewPager =
+                (RotateImageViewPager) findViewById(R.id.content_banner_rotate_image_view_pager);
+        mRotateImageViewPager.setImageViewPagerAdapter(setImageUrl(6));
+    }
+
+    private ArrayList<String> setImageUrl(int imageCount) {
+
+        ArrayList<String> imagePaths = new ArrayList<>();
+        for (int i = 0; i < imageCount; i++) {
+            imagePaths.add("https://unsplash.it/360/220/?random");
+        }
+        return imagePaths;
     }
 }
--- RotateImageViewPager/app/src/main/java/com/example/android/rotateimageviewpager/RotateImageViewPager.java
+++ RotateImageViewPager/app/src/main/java/com/example/android/rotateimageviewpager/RotateImageViewPager.java
@@ -2,13 +2,20 @@ package com.example.android.rotateimageviewpager;
 
 import android.content.Context;
 import android.content.res.TypedArray;
+import android.support.v4.view.PagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.util.AttributeSet;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+
+import java.util.ArrayList;
 
 /**
  * Created by yoon on 2017. 6. 14..
@@ -51,6 +58,8 @@ public class RotateImageViewPager extends RelativeLayout {
     private TextView mTitleTextView;
     private LinearLayout mIndicatorLayout;
 
+    private RotateImageViewPagerAdapter mImageViewPagerAdapter;
+
     private void initializeView(Context context) {
         mRootView = inflate(context, R.layout.layout_rotate_image_view_pager, this);
         mRotateImageViewPager = (ViewPager) mRootView.findViewById(R.id.rotate_image_view_pager);
@@ -62,4 +71,48 @@ public class RotateImageViewPager extends RelativeLayout {
         mIndicatorLayout = (LinearLayout) mRootView.findViewById(R.id.indicator_layout);
     }
 
+    public void setImageViewPagerAdapter(ArrayList<String> imagePaths) {
+        if (mImageViewPagerAdapter == null) {
+            mImageViewPagerAdapter = new RotateImageViewPagerAdapter(imagePaths);
+        }
+        mRotateImageViewPager.setAdapter(mImageViewPagerAdapter);
+    }
+
+    class RotateImageViewPagerAdapter extends PagerAdapter {
+
+        private ArrayList<String> mImagePaths;
+        private LayoutInflater mLayoutInflater;
+
+        public RotateImageViewPagerAdapter(ArrayList<String> imagePaths) {
+            mImagePaths = imagePaths;
+            mLayoutInflater = LayoutInflater.from(getContext());
+        }
+
+        @Override
+        public Object instantiateItem(ViewGroup container, int position) {
+            View view = mLayoutInflater.inflate(R.layout.layout_rotate_image_view, container, false);
+            ImageView imageView = (ImageView) view.findViewById(R.id.rotate_image_view);
+
+            Glide.with(getContext())
+                    .load(mImagePaths.get(position))
+                    .into(imageView);
+            container.addView(view);
+            return view;
+        }
+
+        @Override
+        public int getCount() {
+            return mImagePaths.size();
+        }
+
+        @Override
+        public boolean isViewFromObject(View view, Object object) {
+            return view == object;
+        }
+
+        @Override
+        public void destroyItem(ViewGroup container, int position, Object object) {
+            container.removeView((LinearLayout) object);
+        }
+    }
 }
