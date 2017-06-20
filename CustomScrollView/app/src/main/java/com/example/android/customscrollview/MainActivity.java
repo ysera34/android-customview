3ee9ed272e0bd519770d45f137b3dbb5736e5194
ysera34 <ycyoon34@gmail.com>
2017-06-20 19:42:52 +0900

    add bottom sheet dialog

--- CustomScrollView/app/src/main/java/com/example/android/customscrollview/MainActivity.java
+++ CustomScrollView/app/src/main/java/com/example/android/customscrollview/MainActivity.java
@@ -1,17 +1,21 @@
 package com.example.android.customscrollview;
 
 import android.os.Bundle;
+import android.support.design.widget.BottomSheetBehavior;
+import android.support.design.widget.CoordinatorLayout;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.View;
 import android.view.animation.AccelerateInterpolator;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     private static final String TAG = MainActivity.class.getSimpleName();
 
+    private CoordinatorLayout mCoordinatorLayout;
     private LinearLayout mHeaderLayout;
     private LinearLayout mToolbarLayout;
     private CustomScrollView mCustomScrollView;
@@ -31,6 +35,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        mCoordinatorLayout = (CoordinatorLayout) findViewById(R.id.coordinator_layout);
         mHeaderLayout = (LinearLayout) findViewById(R.id.header_layout);
         mToolbarLayout = (LinearLayout) findViewById(R.id.toolbar_layout);
         mCustomScrollView = (CustomScrollView) findViewById(R.id.custom_scroll_view);
@@ -42,6 +47,8 @@ public class MainActivity extends AppCompatActivity {
         mLayoutInflater = LayoutInflater.from(getApplicationContext());
         mLayoutInflater2 = LayoutInflater.from(getApplicationContext());
         mAddView = (LinearLayout) mLayoutInflater.inflate(R.layout.layout_target, mHeaderLayout, false);
+        mAddView2 = (LinearLayout) mLayoutInflater2.inflate(R.layout.layout_target_2, mHeaderLayout, false);
+        mAddView2.findViewById(R.id.target_text_view_2).setOnClickListener(this);
 
         mCustomScrollView.setOnScrollChangedListener(new CustomScrollView.OnScrollChangedListener() {
             @Override
@@ -57,10 +64,10 @@ public class MainActivity extends AppCompatActivity {
 //                }
 
                 int currentYOffset = scrollView.getScrollY();
-                Log.i(TAG, "current Y Offset : " + currentYOffset);
+//                Log.i(TAG, "current Y Offset : " + currentYOffset);
 
                 float targetLayoutPositionY = mTargetLayout.getY();
-                Log.i(TAG, "targetLayoutPositionY : " + targetLayoutPositionY);
+//                Log.i(TAG, "targetLayoutPositionY : " + targetLayoutPositionY);
 
                 int headerLayoutMeasuredHeight;
                 if (!isTargetSelected) {
@@ -68,7 +75,7 @@ public class MainActivity extends AppCompatActivity {
                 } else {
                     headerLayoutMeasuredHeight = mHeaderLayout.getMeasuredHeight() - mAddView.getMeasuredHeight();
                 }
-                Log.i(TAG, "headerLayoutMeasuredHeight : " + headerLayoutMeasuredHeight);
+//                Log.i(TAG, "headerLayoutMeasuredHeight : " + headerLayoutMeasuredHeight);
 
                 if (currentYOffset >= (int) targetLayoutPositionY - headerLayoutMeasuredHeight) {
                     if (!isTargetSelected) {
@@ -78,7 +85,7 @@ public class MainActivity extends AppCompatActivity {
                         isTargetSelected = true;
                     } else {
                         mHeaderLayout.animate().translationY(-mToolbarLayout.getMeasuredHeight())
-                                .setInterpolator(new AccelerateInterpolator(3));
+                                .setInterpolator(new AccelerateInterpolator(4));
                     }
                 }
                 if (currentYOffset < (int) targetLayoutPositionY - headerLayoutMeasuredHeight) {
@@ -89,7 +96,7 @@ public class MainActivity extends AppCompatActivity {
                         isTargetSelected = false;
                     } else {
                         mHeaderLayout.animate().translationY(0)
-                                .setInterpolator(new AccelerateInterpolator(3));
+                                .setInterpolator(new AccelerateInterpolator(4));
                     }
                 }
 
@@ -97,21 +104,20 @@ public class MainActivity extends AppCompatActivity {
                 int headerLayout2MeasuredHeight = 0;
                 if (isTargetSelected) {
                     if (!isTarget2Selected) {
-                        Log.i(TAG, "isTarget2Selected: " + isTarget2Selected);
+//                        Log.i(TAG, "isTarget2Selected: " + isTarget2Selected);
                         headerLayout2MeasuredHeight = mHeaderLayout.getMeasuredHeight()
                                 - mToolbarLayout.getMeasuredHeight();
-                        Log.i(TAG, "headerLayout2MeasuredHeight: " + headerLayout2MeasuredHeight);
+//                        Log.i(TAG, "headerLayout2MeasuredHeight: " + headerLayout2MeasuredHeight);
                     } else {
-                        Log.i(TAG, "isTarget2Selected: " + isTarget2Selected);
+//                        Log.i(TAG, "isTarget2Selected: " + isTarget2Selected);
                         headerLayout2MeasuredHeight = mHeaderLayout.getMeasuredHeight()
                                 - mToolbarLayout.getMeasuredHeight() - mAddView2.getMeasuredHeight();
-                        Log.i(TAG, "headerLayout2MeasuredHeight: " + headerLayout2MeasuredHeight);
+//                        Log.i(TAG, "headerLayout2MeasuredHeight: " + headerLayout2MeasuredHeight);
                     }
                 }
 
                 if (currentYOffset >= (int) target2LayoutPositionY - headerLayout2MeasuredHeight) {
                     if (!isTarget2Selected) {
-                        mAddView2 = (LinearLayout) mLayoutInflater2.inflate(R.layout.layout_target_2, mHeaderLayout, false);
                         mHeaderLayout.addView(mAddView2);
                         isTarget2Selected = true;
                     }
@@ -125,5 +131,67 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
         });
+
+        mBottomSheetDialog = findViewById(R.id.bottom_sheet_layout);
+        mBottomSheetBehavior = BottomSheetBehavior.from(mBottomSheetDialog);
+
+        mBottomSheetBehavior.setHideable(true);
+        mBottomSheetBehavior.setPeekHeight(0);
+        mBottomSheetBehavior.setState(BottomSheetBehavior.STATE_HIDDEN);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.target_text_view_2:
+                setBottomSheetHeight();
+                wrapInBottomSheet();
+                break;
+        }
+    }
+
+    private BottomSheetBehavior mBottomSheetBehavior;
+    private View mBottomSheetDialog;
+
+    private void wrapInBottomSheet() {
+
+        if (mBottomSheetBehavior.getState() != BottomSheetBehavior.STATE_EXPANDED) {
+//            mBottomSheetDialog.getLayoutParams().height = mCoordinatorLayout.getMeasuredHeight()
+//                    - (mAddView.getMeasuredHeight() + mAddView2.getMeasuredHeight());
+//            mBottomSheetDialog.requestLayout();
+//            mBottomSheetBehavior.onLayoutChild(mCoordinatorLayout, mBottomSheetDialog, ViewCompat.LAYOUT_DIRECTION_LTR);
+
+            mBottomSheetBehavior.setState(BottomSheetBehavior.STATE_EXPANDED);
+            Toast.makeText(getApplicationContext(), "STATE_EXPANDED", Toast.LENGTH_SHORT).show();
+        } else {
+            mBottomSheetBehavior.setState(BottomSheetBehavior.STATE_COLLAPSED);
+            Toast.makeText(getApplicationContext(), "STATE_COLLAPSED", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    public int getStatusBarHeight() {
+        int result = 0;
+        int resourceId = getResources().getIdentifier("status_bar_height", "dimen", "android");
+        if (resourceId > 0) {
+            result = getResources().getDimensionPixelSize(resourceId);
+        }
+        return result;
+    }
+
+    private void setBottomSheetHeight() {
+        mBottomSheetDialog.getLayoutParams().height = mCoordinatorLayout.getMeasuredHeight()
+                - (getStatusBarHeight() + mAddView.getMeasuredHeight() + mAddView2.getMeasuredHeight());
+        mBottomSheetDialog.requestLayout();
+        Toast.makeText(getApplicationContext(), "mBottomSheetDialog.getLayoutParams().height: "
+                + mBottomSheetDialog.getLayoutParams().height, Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (mBottomSheetBehavior.getState() == BottomSheetBehavior.STATE_EXPANDED) {
+            mBottomSheetBehavior.setState(BottomSheetBehavior.STATE_COLLAPSED);
+        } else {
+            super.onBackPressed();
+        }
     }
 }
